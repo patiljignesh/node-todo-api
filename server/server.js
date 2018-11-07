6c43bd07735d3173755da1cdc35404e0d032c700
jigneshkumar <jignesh@liquidmash.io>
2018-11-07 14:50:19 +0200

    Added Express and JSON parser

--- server/server.js
+++ server/server.js
@@ -1,64 +1,28 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// Todo model
-// var Todo = mongoose.model('Todo', {
-//   text:{
-//     type:String,
-//     required: true,
-//     minlength: 1,
-//     trim: true
-//   },
-//   completed:{
-//     type:Boolean,
-//     default: false
-//   },
-//   completedAt:{
-//     type: Number,
-//     default: null
-//   }
-// });
+var app = express();
 
-// var newTodo = new Todo({
-//   text:'Cook dinner'
-// });
-//
-// newTodo.save().then((doc) =>{
-//   console.log('Save to do ', doc);
-// },(error) =>{
-//   console.log('Unable to save Todo', error);
-// });
+app.use(bodyParser.json());
 
-// var otherTodo = new Todo({
-//   text: ' Edit this file '
-// });
-//
-// otherTodo.save().then((doc) =>{
-//   console.log('Save to do ', doc);
-// },(error) =>{
-//   console.log('Unable to save Todo', error);
-// });
+app.post('/todos',(req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+      text: req.body.text
+    });
 
-
-// User Model
-
-var User = mongoose.model('User', {
-  email:{
-    type:String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
+    todo.save().then((doc) =>{
+      res.send(doc);
+    }, (error) =>{
+      console.log(error);
+      res.status(400).send(error);
+    });
 });
 
-var newUser = new User({
-  email: ' test@test.com '
-});
-
-newUser.save().then((doc) =>{
-  console.log('Save user ', doc);
-},(error) =>{
-  console.log('Unable to save user', error);
+app.listen(3000, ()=>{
+  console.log('Started on port 3000');
 });
